9d2088e10dc088b5ff6d22783a20d4f4ee6d8b51
Aleksandr Lukoyanov <liablefish@gmail.com>
2023-01-03 19:59:11 +0200

    Add debug option to solver

--- python/needle/solver.py
+++ python/needle/solver.py
@@ -10,37 +10,51 @@ class SolverMetadata:
     norms: List[float]
 
 
+SolverFunction = Callable[[NDArray], NDArray]
+
+
 class BaseSolver(ABC):
-    def __init__(self):
+    def __init__(self, debug: bool = False):
         self.meta = None
+        self.debug = debug
     
-    def solve(self, op, init_valie: NDArray) -> NDArray:
+    def solve(self, function: SolverFunction, init_value: NDArray) -> NDArray:
         self.meta = SolverMetadata(n_iter=0, norms=[])
-        return self._solve(op, init_valie)
+        return self._solve(function, init_value)
 
     @abstractmethod
-    def _solve(self, op, init_valie: NDArray) -> NDArray:
+    def _solve(self, function: SolverFunction, init_value: NDArray) -> NDArray:
         ...
 
 
 class ForwardIteration(BaseSolver):
-    def __init__(self, *, max_iter: int = 50, tol: float = 1e-4, eps: float = 1e-5):
-        super().__init__()
+    def __init__(
+        self, 
+        *,
+        max_iter: int = 100, 
+        tol: float = 1e-4, 
+        eps: float = 1e-5,
+        debug: bool = False,
+    ):
+        super().__init__(debug)
         self.max_iter = max_iter
         self.tol = tol
         self.eps = eps
 
-    def _solve(self, function: Callable[[NDArray], NDArray], init_value: NDArray) -> NDArray:
+    def _solve(self, function: SolverFunction, init_value: NDArray) -> NDArray:
         prev_value, value = init_value, function(init_value)
         norm = None
         for i in range(self.max_iter):
             norm = array_api.norm(value - prev_value) / (self.eps + array_api.norm(value))
-            self.meta.norms.append(norm.numpy()[0])
+            if self.debug:
+                self.meta.norms.append(norm.numpy()[0])
 
             if norm < self.tol:
                 break
 
             prev_value, value = value, function(value)
 
-        self.meta.n_iter = i
+        if self.debug:
+            self.meta.n_iter = i
+
         return value
